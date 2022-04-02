fa930d68cd53e303a7fcff5684ce5778603ed242
JaxyBLU <tukebaike123@gmail.com>
2022-04-02 23:48:51 +0400

    Moving forward...

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,14 +1,18 @@
 const matheight = document.getElementById('matrix-height');
 const matwidth = document.getElementById('matrix-width');
+const motherofmatrix = document.getElementById('matrix-mother');
 
 const createMatrixButton = document.getElementById('create-matrix');
 
 createMatrixButton.addEventListener('click', createMatrix);
 
 function createMatrix(){
-    for(let i=0; i<matheight.value){
-        for(let j=0; j<matwidth.value){
-            document.createElement('input')
+    for(let i=0; i<matheight.value; i++){
+        for(let j=0; j<matwidth.value; j++){
+            let inputelement = document.createElement('input');
+            inputelement.type = 'number';
+            inputelement.className = 'matrix-input';
+            motherofmatrix.appendChild(inputelement);
         }
     }
 }
