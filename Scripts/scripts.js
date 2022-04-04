e2dfa3689f67f1f51aafc97c47f9c6d1986b02d2
JaxyBLU <tukebaike123@gmail.com>
2022-04-05 02:04:33 +0400

    Getting further, added input value for array dimensions and addeed functionality which creates array with inputs depending on the entered dimensions

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,18 +1,30 @@
 const matheight = document.getElementById('matrix-height');
 const matwidth = document.getElementById('matrix-width');
 const motherofmatrix = document.getElementById('matrix-mother');
-
 const createMatrixButton = document.getElementById('create-matrix');
+
+let matrixInputArray = [];
 
 createMatrixButton.addEventListener('click', createMatrix);
 
 function createMatrix(){
+    motherofmatrix.innerHTML = '';
+
     for(let i=0; i<matheight.value; i++){
+        let firstchild = document.createElement('div');
+        firstchild.className = 'first-child';
         for(let j=0; j<matwidth.value; j++){
             let inputelement = document.createElement('input');
             inputelement.type = 'number';
+            inputelement.max = 9;
+            inputelement.min = -9;
             inputelement.className = 'matrix-input';
-            motherofmatrix.appendChild(inputelement);
+            firstchild.appendChild(inputelement);
         }
+        motherofmatrix.appendChild(firstchild);
     }
+}
+
+function getArrayFromInput(){
+    
 }
