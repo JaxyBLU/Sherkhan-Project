7faed6c0c11a499f63a07a3b4e8788731b82311c
JaxyBLU <tukebaike123@gmail.com>
2022-04-07 01:40:01 +0400

    Some more changes. Check for reports to see what is changed

--- Scripts/chernovik.js
+++ Scripts/chernovik.js
@@ -12,11 +12,10 @@ solutionButton.addEventListener('click', findSolution);
 /* Function for creating the matrix with the width and height values entered by the user */
 function createMatrix(){
     motherofmatrix.innerHTML = ''; /* Need to make sure that previous values are cleared for the parent div */
-    motherofmatrix.style.gridTemplateColumns = `repeat(${matwidth.value}, 1fr)`;
-    var myArray = [...Array(6)].map(e => Array(6));
-    console.log(myArray);
 
     for(let i=0; i<matheight.value; i++){ /* Loop that many times as the height of matrix */
+        let firstchild = document.createElement('div'); /* Creating primary child element to serve as horizontal parent element */
+        firstchild.className = 'first-child'; /* Give it a class name */
         for(let j=0; j<matwidth.value; j++){ /* Loop as many times as the width of matrix */
             let inputelement = document.createElement('input'); /* Creating input element "width" many times */
             inputelement.type = 'number';
@@ -24,8 +23,9 @@ function createMatrix(){
             inputelement.min = -9;
             inputelement.value = 0; /* Default value to 0 to avoid undefined value */
             inputelement.className = 'matrix-input'; /* Give a class name */ 
-            motherofmatrix.appendChild(inputelement);
+            firstchild.appendChild(inputelement); /* And attach it to the horizontal parent element */
         }
+        motherofmatrix.appendChild(firstchild); /* Attach the horizontal parent element to the main mother of the matrix */
     }
 }
 
