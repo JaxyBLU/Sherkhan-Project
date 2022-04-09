d45616b1a46fd565a39f342265b9356e66fa63c2
JaxyBLU <tukebaike123@gmail.com>
2022-04-10 01:13:29 +0400

    Now input and output for matrix values and method choosing is all working

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -2,9 +2,9 @@ const matheight = document.getElementById('matrix-height'); /* height of the mat
 const matwidth = document.getElementById('matrix-width'); /* width of the matrix also entered by the user */
 const motherofmatrix = document.getElementById('matrix-mother'); /* parent div element in which matrix is displayed */
 const createMatrixButton = document.getElementById('create-matrix'); /* button used for creating the matrix */
-const solutionButton = document.getElementById('find-solution');
+const solutionButton = document.getElementById('find-solution'); /* Find the solution button */
+const choosenMethod = document.getElementById('methods'); /* Dropdown menu for method selection */
 
-let matrixInputArray = [];
 
 createMatrixButton.addEventListener('click', createMatrix);
 solutionButton.addEventListener('click', findSolution);
@@ -13,8 +13,6 @@ solutionButton.addEventListener('click', findSolution);
 function createMatrix(){
     motherofmatrix.innerHTML = ''; /* Need to make sure that previous values are cleared for the parent div */
     motherofmatrix.style.gridTemplateColumns = `repeat(${matwidth.value}, 1fr)`;
-    var myArray = [...Array(6)].map(e => Array(6));
-    console.log(myArray);
 
     for(let i=0; i<matheight.value; i++){ /* Loop that many times as the height of matrix */
         for(let j=0; j<matwidth.value; j++){ /* Loop as many times as the width of matrix */
@@ -23,12 +21,23 @@ function createMatrix(){
             inputelement.max = 9;
             inputelement.min = -9;
             inputelement.value = 0; /* Default value to 0 to avoid undefined value */
-            inputelement.className = 'matrix-input'; /* Give a class name */ 
+            inputelement.className = 'matrix-input'; /* Give a class name */
+            inputelement.id = `row${i}-col${j}`;
             motherofmatrix.appendChild(inputelement);
         }
     }
 }
 
 function findSolution(){
+    let matrixInputArray = new Array(matheight);
     
+    for(let x=0; x<matheight.value; x++){
+        matrixInputArray[x] = new Array(matwidth);
+        for(let y=0; y<matwidth.value; y++){
+            matrixInputArray[x][y] = document.getElementById(`row${x}-col${y}`).value;
+            console.log(matrixInputArray[x][y])
+        }
+    }
+
+    console.log(choosenMethod.value);
 }
